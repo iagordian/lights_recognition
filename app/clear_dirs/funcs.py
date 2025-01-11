6e0ad476f3514a8adbdf5d0ccf00716ff852fc59
Игорь Гордиан <114696975+iagordian@users.noreply.github.com>
2025-01-12 01:56:26 +0300

    Add files via upload

--- app/clear_dirs/funcs.py
+++ app/clear_dirs/funcs.py
@@ -1,6 +1,7 @@
 
 
 import os
+import shutil
 
 from app.files_navigation import join_absolute_path, join_file_path, \
     check_exsist
@@ -9,6 +10,26 @@ def clear_dir(dir_to_clear_path: str):
 
     dir_to_clear_path = join_absolute_path(dir_to_clear_path)
     clear_dir_body(dir_to_clear_path)
+
+def clear_pycache_files():
+
+    dir = os.getcwd()
+    clear_pyche_files_body(dir)
+
+def clear_pyche_files_body(dir_path):
+
+    list_dir = os.listdir(dir_path)
+    
+    for file_name in list_dir:
+        file_path = join_file_path(dir_path, file_name)
+
+        if os.path.isdir(file_path):
+    
+            if file_name == '__pycache__':
+                shutil.rmtree(file_path)
+
+            else:
+                clear_pyche_files_body(file_path)
 
 def clear_dir_body(dir_path):
 
--- app/clear_dirs/scripts.py
+++ app/clear_dirs/scripts.py
@@ -1,7 +1,7 @@
 
 from functools import partial
 
-from .funcs import clear_dir
+from .funcs import clear_dir, clear_pycache_files
 
 
 clear_test_model_outputs = partial(clear_dir, dir_to_clear_path='ML_tests/output')
